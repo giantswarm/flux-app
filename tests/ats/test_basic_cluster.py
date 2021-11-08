73d6bc2eb4d0aa993c743d7e517559f9fde3f1c6
Łukasz Piątkowski <piontec@gmail.com>
2021-11-08 16:06:06 +0100

    add missing catalog creation

--- tests/ats/test_basic_cluster.py
+++ tests/ats/test_basic_cluster.py
@@ -5,6 +5,8 @@ import pykube
 import pytest
 from pytest_helm_charts.fixtures import Cluster
 
+from pytest_helm_charts.giantswarm_app_platform.catalog import CatalogFactoryFunc
+
 # noinspection PyUnresolvedReferences
 from fixtures import flux_deployments  # noqa: F104
 from utils import get_git_repository_obj, get_kustomize_obj
@@ -33,17 +35,20 @@ def test_pods_available(kube_cluster: Cluster, flux_deployments: List[pykube.Dep
 
 
 @pytest.mark.functional
-def test_kustomization_works(kube_cluster: Cluster, flux_deployments: List[pykube.Deployment]) -> None:
+def test_kustomization_works(kube_cluster: Cluster, flux_deployments: List[pykube.Deployment],
+                             catalog_factory: CatalogFactoryFunc) -> None:
+    namespace = "default"
+    catalog_factory("giantswarm", namespace, "https://giantswarm.github.io/giantswarm-catalog/")
     git_repo_cr_name = "flux-app-tests"
     # todo: turn into fixture
-    git_repo = get_git_repository_obj(kube_cluster.kube_client, git_repo_cr_name, "default",
+    git_repo = get_git_repository_obj(kube_cluster.kube_client, git_repo_cr_name, namespace,
                                       "1m", "https://github.com/giantswarm/flux-app-tests",
                                       "feature/init-with-data", ignore_pattern="tests/test_cases/**/result")
     git_repo.create()
     # TODO: wait for repo to be ready
     test_name = "simple-app-cr-delivery"
     # todo: turn into fixture
-    ks = get_kustomize_obj(kube_cluster.kube_client, test_name, "default",
+    ks = get_kustomize_obj(kube_cluster.kube_client, test_name, namespace,
                            True, "1m", f"./tests/test_cases/{test_name}", git_repo_cr_name, "2m")
     ks.create()
     # TODO: wait for kustomization to show up
