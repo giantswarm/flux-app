14ca7107cc2cbe60bb3d2d35c3c45daf8dd6a33c
Łukasz Piątkowski <piontec@gmail.com>
2021-12-03 12:41:27 +0100

    extract common assert

--- tests/ats/test_basic_cluster.py
+++ tests/ats/test_basic_cluster.py
@@ -4,6 +4,7 @@ from typing import List
 
 import pykube
 import pytest
+from pykube import HTTPClient
 from pykube.exceptions import HTTPError
 from pytest_helm_charts.fixtures import Cluster
 from pytest_helm_charts.giantswarm_app_platform.catalog import CatalogFactoryFunc
@@ -23,6 +24,21 @@ from fixtures_helpers import KustomizationFactoryFunc, GitRepositoryFactoryFunc,
 logger = logging.getLogger(__name__)
 
 APP_DEPLOYMENT_TIMEOUT_SEC = 180
+
+
+def assert_hello_world_is_running(kube_client: HTTPClient, app_namespace: str):
+    app_svc_name = "hello-world-service"
+    app_deploy_name = "hello-world"
+    app_svc_port = 8080
+    wait_for_deployments_to_run(
+        kube_client,
+        [app_deploy_name],
+        app_namespace,
+        APP_DEPLOYMENT_TIMEOUT_SEC,
+    )
+    app_svc: pykube.Service = pykube.Service.objects(kube_client, namespace=app_namespace).get(name=app_svc_name)
+    response = app_svc.proxy_http_get("/", app_svc_port)
+    assert response.status_code == 200
 
 
 @pytest.mark.smoke
@@ -47,8 +63,8 @@ def test_pods_available(
         logger.info(f"Deployment '{d.name}' is ready")
 
 
-# @pytest.mark.functional
-# @pytest.mark.upgrade
+@pytest.mark.functional
+@pytest.mark.upgrade
 @pytest.mark.parametrize(
     "test_name", ["simple-app-cr-delivery", "simple-chart-release"]
 )
@@ -108,23 +124,7 @@ def test_kustomization_works(
         "2m",
     )
 
-    # now we wait for the app to be deployed by flux and to run
-    app_namespace = "hello-world"
-    app_svc_name = "hello-world-service"
-    app_deploy_name = "hello-world"
-    app_svc_port = 8080
-    wait_for_deployments_to_run(
-        kube_cluster.kube_client,
-        [app_deploy_name],
-        app_namespace,
-        APP_DEPLOYMENT_TIMEOUT_SEC,
-    )
-    app_svc: pykube.Service = pykube.Service.objects(
-        kube_cluster.kube_client, namespace=app_namespace
-    ).get(name=app_svc_name)
-
-    response = app_svc.proxy_http_get("/", app_svc_port)
-    assert response.status_code == 200
+    assert_hello_world_is_running(kube_cluster.kube_client, "hello-world")
 
 
 @pytest.mark.functional
@@ -157,19 +157,4 @@ def test_helm_works(
                          },
                          interval="1m")
 
-    # now we wait for the app to be deployed by flux and to run
-    app_svc_name = "hello-world-service"
-    app_deploy_name = "hello-world"
-    app_svc_port = 8080
-    wait_for_deployments_to_run(
-        kube_cluster.kube_client,
-        [app_deploy_name],
-        app_namespace,
-        APP_DEPLOYMENT_TIMEOUT_SEC,
-    )
-    app_svc: pykube.Service = pykube.Service.objects(
-        kube_cluster.kube_client, namespace=app_namespace
-    ).get(name=app_svc_name)
-
-    response = app_svc.proxy_http_get("/", app_svc_port)
-    assert response.status_code == 200
+    assert_hello_world_is_running(kube_cluster.kube_client, app_namespace)
