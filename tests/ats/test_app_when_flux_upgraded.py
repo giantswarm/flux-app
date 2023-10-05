85abcf046f3f901e31c1f9d95b8200578cf9e06b
Laszlo Uveges <laszlo@giantswarm.io>
2023-10-05 14:53:54 +0200

    Test flux-app-tests changes

--- tests/ats/test_app_when_flux_upgraded.py
+++ tests/ats/test_app_when_flux_upgraded.py
@@ -148,7 +148,7 @@ def deploy_as_kustomization(
         namespace,
         "1m",
         "https://github.com/giantswarm/flux-app-tests",
-        "main",
+        "align-to-flux-v2-updated-flux-app-repo",
         ignore_pattern="tests/test_cases/**/result",
     )
     git_repository.create()
